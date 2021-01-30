9fec20ee419193d2c7e3de2dfe998725a44e5361
Arman <armansidhu3@gmail.com>
2021-01-31 01:48:43 +0530

    Added thumbnail

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -3,6 +3,9 @@ import React from "react";
 const Wrapper = (props) => {
     return (
         <div className="wrapper">
+            <div className="thumbnail">
+                <img src="../../public/logo512.png" alt=""/>
+            </div>
             {props.children}
         </div>
     );
